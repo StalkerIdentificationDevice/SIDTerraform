c41012df2bbfdd8e1be40c9fa273e5da564bc6c2
bgolden1 <brodygolden7@gmail.com>
2023-05-19 01:20:58 -0400

    initial terraform runs with provisioned table

--- main.tf
+++ main.tf
@@ -11,27 +11,93 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+resource "aws_dynamodb_table" "user-tracking" {
+  name         = "user-tracking"
+  hash_key     = "User"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "User"
+    type = "S"
+  }
+  point_in_time_recovery { enabled = false }
+  server_side_encryption { enabled = true }
+}
 
-  function_name = "process_images"
-  description   = "Lambda Function to process images from mobile devices"
-  handler       = "process_images.process"
-  runtime       = "python3.8"
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
 
-  source_path = "./lambda"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
 
-  tags = {
-    Name = "process_images"
+    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_s3_bucket" "photo_bucket" {
-  bucket = "sid-user-public-photo-data"
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_s3_access_point" "photo_bucket" {
-  bucket = aws_s3_bucket.photo_bucket.id
-  name   = "sid-user-public-photo-data"
-  policy = ""
+resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
+  name = "dynamodb_lambda_policy"
+  role = aws_iam_role.iam_for_lambda.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : ["dynamodb:*"],
+        "Resource" : "${aws_dynamodb_table.user-tracking.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "rekognition-lambda-policy" {
+  name = "rekognition_lambda_policy"
+  role = aws_iam_role.iam_for_lambda.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : ["rekognition:*"],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.func.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+resource "aws_lambda_function" "func" {
+  filename      = "lambda.zip"
+  function_name = "process_images"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "process_images.lambda_handler"
+  runtime       = "python3.10"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket_prefix = "sid-user-public-photo-data"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.func.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
